513ce7c202dcac865cfecdb1df8f3527c7053fc3
AGENT911147 <luckynick5000@gmail.com>
2026-01-07 00:42:50 +0100

    Update bot and owner details in settings

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'steve', // ur bot name
+  OWNER_NAME: 'AGENT911', //your name
+  OWNER_NUMBER: '2349118743413', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
